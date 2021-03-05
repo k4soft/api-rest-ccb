be4c52e68382eea80ece5129b2d6472247707f50
k4soft <info@k4soft.com>
2021-03-04 19:45:58 -0500

    se adicionan manejadores y excepciones de negocio

---
+++ apirest/src/main/java/co/com/ccb/apirest/aplicacion/impl/ManejadorComandoRespuesta.java
@@ -0,0 +1,5 @@
+package co.com.ccb.apirest.aplicacion.impl;
+
+public interface ManejadorComandoRespuesta {
+
+}
---
+++ apirest/src/main/java/co/com/ccb/apirest/dominio/ValidadorArgumento.java
@@ -0,0 +1,5 @@
+package co.com.ccb.apirest.dominio;
+
+public class ValidadorArgumento {
+
+}
